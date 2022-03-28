9c712977a4ede66b2e5dad7b2f90e607400874d4
Mohsin Hayat <mohsinhayat104@gmail.com>
2022-03-29 01:51:41 +0500

    add initial structure of the site

---
+++ pages/_app.js
@@ -0,0 +1,26 @@
+import 'nextra-theme-blog/style.css'
+import Head from 'next/head'
+
+import '../styles/main.css'
+
+export default function Nextra({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS"
+          href="/feed.xml"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
---
+++ pages/posts/index.md
@@ -0,0 +1,7 @@
+---
+type: posts
+title: Posts
+date: 2022-03-29
+---
+
+# Posts
